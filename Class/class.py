590b2e89f561b74e2d9f615003604cc81f6821c2
Ki deuk Park <zxcv4778@gmail.com>
2019-05-23 18:01:08 +0900

    class 진행 중...

---
+++ Class/class.py
@@ -0,0 +1,20 @@
+# class 변수
+
+# class Service:
+#     secret = "영구는 배꼽이 두 개다."
+#
+# pey = Service()
+#
+# print(pey.secret)
+
+# class 함수
+
+class Service:
+    secret = "영구는 배꼽이 두 개다."
+
+    def sum(self, a, b):
+        result = a + b
+        print("%s + %s = %s입니다." %(a, b, result))
+
+pey = Service()
+pey.sum(1, 1)
